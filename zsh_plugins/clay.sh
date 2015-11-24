b2c91b8673891f442c39ca40bdc9e232e659ef55
Clay Schubiner <clay.schubiner@airbnb.com>
2015-11-23 17:01:28 -0800

    morarar

--- zsh_plugins/clay.sh
+++ zsh_plugins/clay.sh
@@ -1,13 +1,7 @@
 export PATH=$PATH:~/bin:~/Dropbox/googleSearchScraper/bin
 export PATH=$PATH:/opt/local/bin
 
-
 export SSH_AUTH_SOCK=$TMPDIR/ssh-agent-$USER.sock
-
-function goldkey2 {
-	 killall ssh-agent
-	 ssh-add -s /usr/local/lib/opensc-pkcs11.so -t 8640000
-}
 
 alias air='cd ~/airlab/repos/airbnb'
 alias data='cd ~/airlab/repos/data'
@@ -16,7 +10,11 @@ alias rookery='cd ~/airlab/repos/rookery'
 alias lend='cd ~/lending_club'
 
 alias git=hub
+alias gm='git commit'
 alias gst='git status'
-alias tm="git status -s | egrep '(([M|A][M|A])|( [M|A])|([M|A] )) ' | cut  -c 4- | xargs -n1 touch"
-alias gps="git rev-parse --abbrev-ref HEAD | awk '{print \" push origin \"$1\":\"$1}' | xargs git"
-alias gpsf="git rev-parse --abbrev-ref HEAD | awk '{print \" push origin \"$1\":\"$1\" -f\"}' | xargs git"
+alias gps="git rev-parse --abbrev-ref HEAD | awk '{print \"git push origin \"$1\":\"$1}' | sh"
+alias gpsf="git rev-parse --abbrev-ref HEAD | awk '{print \"git push origin \"$1\":\"$1\" -f\"}' | sh"
+
+alias gsm="git status -s | egrep '(([M|A][M|A])|( [M|A])|([M|A] )) ' | cut  -c 4-"
+alias gam="gsm | xargs -n1 git add"
+alias tm="gsm | xargs -n1 touch"
